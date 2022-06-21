d1a3ca1eedb215618f9cafccc0a0ed1b9b5a120a
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-06-21 23:05:27 +0300

    changing component

--- src/views/RickAndMortyCharacter.vue
+++ src/views/RickAndMortyCharacter.vue
@@ -22,7 +22,8 @@ export default {
     try {
       const {id} = this.$route.params
       if (id) {
-        this.character = await this.$store.dispatch('fetchSingleCharacter', id)
+        const {data} = await this.$store.dispatch('fetchSingleCharacter', id)
+        this.character = data
       }
     } catch (err) {
       console.log(err)
@@ -35,21 +36,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<!--<style scoped>-->
-<!--h1, h2 {-->
-<!--  font-weight: normal;-->
-<!--}-->
-<!--ul {-->
-<!--  list-style-type: none;-->
-<!--  padding: 0;-->
-<!--}-->
-<!--li {-->
-<!--  display: inline-block;-->
-<!--  margin: 0 10px;-->
-<!--}-->
-<!--a {-->
-<!--  color: #42b983;-->
-<!--}-->
-<!--</style>-->
